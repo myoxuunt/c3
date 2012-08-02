8a42320cc2c26cd01b0107aa120f342579a25e90
hkiaipc <hkiaipc@163.com>
2012-08-02 09:28:44 +0800

    add xdgk.common.interface project

---
+++ 8.Src/Xdgk.Communi.Interface/DevicePluginInterface.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xdgk.Communi.Interface
+{
+    /// <summary>
+    /// remove to communi.dll?
+    /// </summary>
+    public interface DevicePluginInterface
+    {
+        DeviceFactory GetDeviceFactory();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface DeviceFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string ForDeviceType { get; }
+        // device create()
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface DeviceUI
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string ForDeviceType { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="station"></param>
+        /// <returns></returns>
+        object AddDevice(object station);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="device"></param>
+        void EditDevice(object device);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="device"></param>
+        /// <returns></returns>
+        bool DeleteDevice(object device);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface DevcieProcessor
+    {
+
+    }
+}
