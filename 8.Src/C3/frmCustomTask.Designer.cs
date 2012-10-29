bdf1445987cabe53471b078c05a69fa4c22fb7f9
hkiaipc <hkiaipc@163.com>
2012-10-29 14:37:48 +0800

    add custom task form

---
+++ 8.Src/C3/frmCustomTask.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace C3
+{
+    partial class frmCustomTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSend = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Location = new System.Drawing.Point(258, 152);
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(346, 152);
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(300, 21);
+            this.txtName.TabIndex = 19;
+            // 
+            // lblName
+            // 
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(100, 21);
+            this.lblName.TabIndex = 20;
+            this.lblName.Text = "Name:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSend
+            // 
+            this.lblSend.Location = new System.Drawing.Point(12, 36);
+            this.lblSend.Name = "lblSend";
+            this.lblSend.Size = new System.Drawing.Size(100, 21);
+            this.lblSend.TabIndex = 22;
+            this.lblSend.Text = "Send:";
+            this.lblSend.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(118, 36);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(300, 104);
+            this.txtSend.TabIndex = 21;
+            // 
+            // frmCustomTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 188);
+            this.Controls.Add(this.lblSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmCustomTask";
+            this.Text = "frmCustomTask";
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.lblName, 0);
+            this.Controls.SetChildIndex(this.okButton, 0);
+            this.Controls.SetChildIndex(this.cancelButton, 0);
+            this.Controls.SetChildIndex(this.txtSend, 0);
+            this.Controls.SetChildIndex(this.lblSend, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSend;
+        private System.Windows.Forms.TextBox txtSend;
+    }
+}
---
+++ 8.Src/C3/frmCustomTask.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+using Xdgk.Common;
+using C3.Communi;
+
+namespace C3
+{
+    public partial class frmCustomTask : NUnit.UiKit.SettingsDialogBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public frmCustomTask()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            //this.DialogResult = DialogResult.OK;
+            this._device.TaskManager.Tasks.Add(this.Task);
+            this.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte[] SendBytes
+        {
+            get
+            {
+                byte[] bs = Xdgk.Common.HexStringConverter.HexStringToBytes(this.txtSend.Text);
+                return bs;
+            }
+            set
+            {
+                string txt = HexStringConverter.BytesToHexString(value);
+                this.txtSend.Text = txt;
+            }
+        }
+
+        #region Device
+        /// <summary>
+        /// 
+        /// </summary>
+        public IDevice Device
+        {
+            get
+            {
+                if (_device == null)
+                {
+                }
+                return _device;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Device");
+                }
+                _device = value;
+            }
+        } private IDevice _device;
+        #endregion //Device
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ITask Task
+        {
+            get
+            {
+                TimeSpan timeout  = TimeSpan .FromMilliseconds ( 
+                    this._device.Station.CommuniPortConfig.TimeoutMilliSecond );
+
+                ITask task = new Task(
+                    this._device,
+                    this.Op,
+                    Strategy.CreateImmediateStrategy(),
+                   timeout);
+                return task;
+            }
+        }
+
+        IOpera Op
+        {
+            get
+            {
+                Opera o = new Opera(this._device.DeviceType.Type.Name, "noname");
+                DataField df1 = new DataField("df1", 0, SendBytes.Length, null);
+                o.SendPart.DataFieldManager.DataFields.Add(df1);
+                return o;
+            }
+        }
+    }
+}
