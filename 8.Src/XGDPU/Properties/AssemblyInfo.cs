9d94d33cad5733372cfb78ac932f00b9647bdfb5
hkiaipc <hkiaipc@163.com>
2012-12-17 16:43:08 +0800

    modify xg dpu assembly version

--- 8.Src/XGDPU/Properties/AssemblyInfo.cs
+++ 8.Src/XGDPU/Properties/AssemblyInfo.cs
@@ -32,8 +32,13 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+
+// v1.2.0.0     2012-12-17
+//      tm save to tblTM not tblCard
+//      xgdata save to tblTMData not tblXgData
+//
 
 // v1.1.0.0     2012-12-10
 //      fixed xgdata getReportItemCollection bug
