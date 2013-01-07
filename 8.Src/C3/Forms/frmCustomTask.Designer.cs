ce9bdea51a66fb2b73611ad0db89f14b4541834e
hkiaipc <hkiaipc@163.com>
2013-01-07 13:39:02 +0800

    move frmCustomTask and frmDeviceHistoryData to Forms directory

---
+++ 8.Src/C3/Forms/frmCustomTask.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace C3
+{
+    partial class frmCustomTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSend = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReceived = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(424, 36);
+            this.okButton.Text = "发送";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(424, 210);
+            this.cancelButton.Text = "关闭";
+            this.cancelButton.Visible = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(300, 21);
+            this.txtName.TabIndex = 19;
+            this.txtName.Text = "custom";
+            // 
+            // lblName
+            // 
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(100, 21);
+            this.lblName.TabIndex = 20;
+            this.lblName.Text = "名称:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSend
+            // 
+            this.lblSend.Location = new System.Drawing.Point(12, 36);
+            this.lblSend.Name = "lblSend";
+            this.lblSend.Size = new System.Drawing.Size(100, 21);
+            this.lblSend.TabIndex = 22;
+            this.lblSend.Text = "发送(Hex):";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(118, 36);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(300, 42);
+            this.txtSend.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "接收(Hex):";
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Location = new System.Drawing.Point(118, 84);
+            this.txtReceived.Multiline = true;
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.ReadOnly = true;
+            this.txtReceived.Size = new System.Drawing.Size(300, 150);
+            this.txtReceived.TabIndex = 23;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 244);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(505, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 25;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssStatus
+            // 
+            this.tssStatus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tssStatus.Name = "tssStatus";
+            this.tssStatus.Size = new System.Drawing.Size(490, 17);
+            this.tssStatus.Spring = true;
+            this.tssStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmCustomTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 266);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.lblSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmCustomTask";
+            this.Text = "自定义";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmCustomTask_FormClosed);
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.lblName, 0);
+            this.Controls.SetChildIndex(this.okButton, 0);
+            this.Controls.SetChildIndex(this.cancelButton, 0);
+            this.Controls.SetChildIndex(this.txtSend, 0);
+            this.Controls.SetChildIndex(this.lblSend, 0);
+            this.Controls.SetChildIndex(this.txtReceived, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.statusStrip1, 0);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReceived;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssStatus;
+    }
+}
