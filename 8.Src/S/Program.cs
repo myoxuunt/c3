93c3af3328123dd70b08bd94153348315b522f37
35982772 <35982772@qq.com>
2013-06-04 17:20:27 +0800

    s ...

--- 8.Src/S/Program.cs
+++ 8.Src/S/Program.cs
@@ -24,9 +24,19 @@ namespace S
         /// 
         /// </summary>
         /// <param name="communiPort"></param>
-        public Client(ICommuniPort communiPort )
+        public Client(ICommuniPort communiPort)
         {
             this.CommuniPort = communiPort;
+            this.CommuniPort.Received += new EventHandler(CommuniPort_Received);
+        }
+
+        void CommuniPort_Received(object sender, EventArgs e)
+        {
+            ICommuniPort cp = (ICommuniPort )sender ;
+            byte[] bs = cp.Read();
+
+            // TODO: 
+            //
         }
 
         /// <summary>
@@ -44,5 +54,106 @@ namespace S
                 _communiPort = value;
             }
         } private ICommuniPort _communiPort;
+    }
+
+    public class ClientCollection : Collection<Client>
+    {
+    }
+
+    public class ClientEventArgs: EventArgs 
+    {
+        public ClientEventArgs ( Client client )
+        {
+            this.Client = client;
+        }
+
+        #region Client
+/// <summary>
+/// 
+/// </summary>
+public Client Client
+{
+	get
+	{
+		return _client;
+	}
+	set
+	{
+		_client = value;
+	}
+} private Client _client;
+#endregion //Client
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ClientEventHandler ( object sender , ClientEventArgs e );
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClientManager
+    {
+        public event ClientEventHandler Added;
+        public event ClientEventHandler Removed;
+        #region Clients
+        /// <summary>
+        /// 
+        /// </summary>
+        public ClientCollection Clients
+        {
+            get
+            {
+                if (_clients == null)
+                {
+                    _clients = new ClientCollection();
+                }
+                return _clients;
+            }
+        } private ClientCollection _clients;
+        #endregion //Clients
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="client"></param>
+        public void Add(Client client)
+        {
+            this.Clients.Add(client);
+            if (Added != null)
+            {
+                Added(this, new ClientEventArgs(client));
+            }
+        }
+
+        internal void RemoveByCommuniPort(ICommuniPort cp)
+        {
+            for( int i = this.Clients.Count -1 ; i>=0 ; i-- )
+            {
+                Client c = this.Clients[i];
+                if ( c .CommuniPort == cp )
+                {
+                    this.Remove(c);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        private void Remove(Client c)
+        {
+            this.Clients.Remove(c);
+
+            if (Removed != null)
+            {
+                Removed(this, new ClientEventArgs(c));
+            }
+        }
     }
 }
--- 8.Src/S/SApp.cs
+++ 8.Src/S/SApp.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using Xdgk.Common;
@@ -8,6 +9,21 @@ namespace S
 {
     public class SApp : AppBase
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static public SApp App
+        {
+            get
+            {
+                if (AppBase.DefaultInstance == null)
+                {
+                    AppBase.DefaultInstance = new SApp();
+                }
+                return AppBase.DefaultInstance as SApp;
+            }
+        }
 
         /// <summary>
         /// 
@@ -24,24 +40,7 @@ namespace S
             }
         } private FrmMain _mainForm;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        static public SApp App
-        {
-            get
-            {
-                if (AppBase.DefaultInstance == null)
-                {
-                    AppBase.DefaultInstance = new SApp();
-                }
-                return AppBase.DefaultInstance as SApp;
-            }
-        }
-    }
 
-    public class Soft
-    {
         public SocketListenerManager SocketListenerManager
         {
             get
@@ -52,42 +51,123 @@ namespace S
                     string path = PathUtils.SocketListenerConfigFileName;
                     XmlSocketListenBuilder builder = new XmlSocketListenBuilder(path);
                     builder.Build(_socketListenerManager);
+
+                    _socketListenerManager.NewCommuniPortEvent += new EventHandler(_socketListenerManager_NewCommuniPortEvent);
                 }
                 return _socketListenerManager;
             }
         } private SocketListenerManager _socketListenerManager;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _socketListenerManager_NewCommuniPortEvent(object sender, EventArgs e)
+        {
+            SocketListenerManager sckListenMan = sender as SocketListenerManager;
+            ICommuniPort cp = sckListenMan.NewCommuniPort;
+            this.CommuniPortManager.Add(cp);
+        }
 
-        #region ISoft ≥…‘±
+
 
         #region CommuniPortManager
+        /// <summary>
+        /// 
+        /// </summary>
         public CommuniPortManager CommuniPortManager
         {
             get
             {
                 if (_communiPortManager == null)
                 {
-                    _communiPortManager = new CommuniPortManager(this);
+                    _communiPortManager = new CommuniPortManager();
                 }
                 return _communiPortManager;
             }
         } private CommuniPortManager _communiPortManager;
         #endregion //CommuniPortManager
 
-        #region ErrorManager
-        public ErrorManager ErrorManager
+        #region ClientManager
+        /// <summary>
+        /// 
+        /// </summary>
+        public ClientManager ClientManager
         {
             get
             {
-                if (_errorManager == null)
+                if (_clientManager == null)
                 {
-                    _errorManager = new ErrorManager(this);
+                    _clientManager = new ClientManager();
                 }
-                return _errorManager;
+                return _clientManager;
             }
-        } private ErrorManager _errorManager;
-        #endregion //
+        } private ClientManager _clientManager;
+        #endregion //ClientManager
 
-        #endregion
+        //#region ErrorManager
+        //public ErrorManager ErrorManager
+        //{
+        //    get
+        //    {
+        //        if (_errorManager == null)
+        //        {
+        //            _errorManager = new ErrorManager(this);
+        //        }
+        //        return _errorManager;
+        //    }
+        //} private ErrorManager _errorManager;
+        //#endregion //
+
+    }
+
+    internal class CommuniPortEventProcessor
+    {
+        private CommuniPortManager _cpManager;
+        private ClientManager _clientManager;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cpManager"></param>
+        internal CommuniPortEventProcessor(CommuniPortManager cpManager)
+        {
+            Debug.Assert(cpManager != null);
+            this._cpManager = cpManager;
+
+
+            _cpManager.AddedCommuniPort += new CommuniPortEventHandler(_cpManager_AddedCommuniPort);
+            _cpManager.ClosedCommuniPort += new CommuniPortEventHandler(_cpManager_ClosedCommuniPort);
+            _cpManager.DeterminedCommuniPort += new CommuniPortEventHandler(_cpManager_DeterminedCommuniPort);
+            _cpManager.ReceivedCommuniPort += new CommuniPortEventHandler(_cpManager_ReceivedCommuniPort);
+            _cpManager.RemovedCommuniPort += new CommuniPortEventHandler(_cpManager_RemovedCommuniPort);
+        }
+
+        void _cpManager_RemovedCommuniPort(object sender, CommuniPortEventArgs e)
+        {
+            this._clientManager.RemoveByCommuniPort(e.CommuniPort);
+        }
+
+        void _cpManager_ReceivedCommuniPort(object sender, CommuniPortEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void _cpManager_DeterminedCommuniPort(object sender, CommuniPortEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void _cpManager_ClosedCommuniPort(object sender, CommuniPortEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void _cpManager_AddedCommuniPort(object sender, CommuniPortEventArgs e)
+        {
+            Client c = new Client(e.CommuniPort);
+            this._clientManager.Add(c);
+        }
     }
 }
